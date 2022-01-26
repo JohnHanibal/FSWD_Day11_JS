d7f1590263ef7c1df872784b177e82a495a3fe68
JohnHanibal <Johannes_aho@outlook.com>
2022-01-26 10:01:20 +0100

    second challange finish

---
+++ Challange_2/script.js
@@ -0,0 +1,4 @@
+let a = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up"
+
+console.log(a.split("$").join(" "));
+
