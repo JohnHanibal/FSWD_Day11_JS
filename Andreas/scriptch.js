cd8e2d56d5f5b6306c832d3cea17920a088b186a
AndreasPlangger <andreas_plangger@hotmail.com>
2022-01-26 15:51:46 +0100

    bugfixes

---
+++ Andreas/scriptch.js
@@ -0,0 +1,20 @@
+var vienna = [
+["Vienna"+' ',"is"+' ',"a"+' ',"nice"+' ',"city."],
+["      "+' ',"is"+' ',"a"+' ',"nice"+' ',"city."],
+["Vienna"+' ',"is"+' ',"a"+' ',"nice"+' ',"city."],
+["Vienna"+' ',"is"+' ',"a"+' ',"nice"+' ',"city."],
+["Vienna"+' ',"is"+' ',"a"+' ',"nice"+' ',"city."]
+];
+
+
+
+    
+    document.write(vienna[0][0]);
+    document.write("<br>");
+    document.write(vienna[1][1]);
+    document.write("<br>");
+    document.write(vienna[2][2]);
+    document.write("<br>");
+    document.write(vienna[3][3]);
+    document.write("<br>");
+    document.write(vienna[4][4]);
